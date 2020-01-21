200c9313c90bb5b451a694ea6e119a3c58417c78
PierreEd <jacquetpierreedouard@gmail.com>
2020-01-21 10:48:43 +0000

    rewrote few stuffs

--- Minuit/test_iminuit.py
+++ Minuit/test_iminuit.py
@@ -8,7 +8,6 @@ from iminuit import Minuit
 import matplotlib.pyplot as plt 
 from tqdm import tqdm 
 import math
-#Define names #
 
 
 #Define boolean sequence of variables to keep fixed 
@@ -70,18 +69,7 @@ amplitude_names = [
     'a_00_r_im',
 ]
 
-#Number of step per fit (migrad)
-Ncall=1000
 
-
-def nll(signal_coeffs):
-    """Get the normalized negative log likelihood
-    Working with the normalised version ensures we don't need to re-optimize hyper-parameters when we
-    change signal event numbers.
-    Returns:
-        Scalar tensor
-    """
-    return bmfs.normalized_nll(signal_coeffs, signal_events)
 
 def LaTex_labels(amplitude_latex_names):
     LaTex_labels=[]
@@ -90,7 +78,6 @@ def LaTex_labels(amplitude_latex_names):
         for j in param:
             LaTex_labels.append(j+i+')')
     return LaTex_labels
-
 
 def Standard_labels(amplitude_names):
     labels=[]
@@ -106,62 +93,8 @@ def set_coef(Coef_INIT , Coef0,  fix_array ) :
             Coef_INIT[i]=  Coef0[i]
     return Coef_INIT
 
-def plot_fixed(m , Coef0 ,fix_array , amplitude_latex_names, coeffout,show = True, save = False):
-    #First find index of non fixed parameters 
-    ind=[]
-    for i in range(len(fix_array)):
-        if fix_array[i]==0 : 
-            ind.append(int(i))
 
-    #Now ,plot nll profile for given parameters
-    LaTex=LaTex_labels(amplitude_latex_names)
-    Title=Standard_labels(amplitude_names)
-    n=int(math.ceil(np.sqrt(len(ind))))
-    fig, axs = plt.subplots(n, n)
-    n1 , n2 = 0 , 0 
-
-    #Different case if only one plot or more
-    if len(ind)==1 : 
-        i0=ind[0]
-        X , Y =m.profile(m.parameters[i0])
-        axs.plot(X , Y , label='NLL')
-        ymin, ymax = axs.get_ylim()
-        axs.vlines(Coef0[i0] , ymin , ymax , label='init value')
-        axs.vlines(m.values[i0] , ymin , ymax , label='fitted value' , color='k')
-        X , Y =m.profile(m.parameters[i0])
-        axs.set(xlabel=LaTex[i0], ylabel=r'NLL')
-        Xmin , Xmax = m.values[i0]-m.errors[i0] ,  m.values[i0]+m.errors[i0] 
-        axs.axvspan(Xmin, Xmax, alpha=0.1, color='red' , label= r'$\pm \sigma$')
-        axs.set_title('Fit for '+LaTex[i0]+'with N='+str(Ncall)+'\n Expected Value:'+str(Coef0[i0])+' Actual Value:'+str(coeffout[i0]))
-        axs.legend()
-        Title = LaTex[i0]
-    else : 
-        for i in ind:
-            X , Y =m.profile(m.parameters[i])
-            #ax=axs[n1 , n2]
-            fig, ax = plt.subplots(1, 1)
-            ax.plot(X , Y , label='NLL')
-            ymin, ymax = ax.get_ylim()
-            ax.vlines(Coef0[i] , ymin , ymax , label='init value')
-            ax.set(xlabel=LaTex[i], ylabel=r'NLL')
-            Xmin , Xmax = m.values[i]-m.errors[i] ,  m.values[i]+m.errors[i] 
-            ax.axvspan(Xmin, Xmax, alpha=0.1, color='red' , label= r'$\pm \sigma$')
-            #ax.set_title(LaTex[i])
-            ax.legend()
-            plt.savefig('./tests/Test/'+Title[i]+'.png')
-            n2+=1
-            if n2==n:
-                n2=0
-                n1+=1
-        #Title = 'Likelihood profiles for '+str(len(ind))+' parameters'
-    plt.tight_layout()
-    '''
-    if save:
-        plt.savefig('./tests/Test/'+Title+'.png')
-    if show:
-        plt.show()
-    '''
-def generate(events = 24000, fix_array = fix_array, verbose = False):
+def generate_SM(events = 2400, fix_array = fix_array, verbose = False):
     """
     Generate Data for fitting
     Returns Amplitude Coefficients, and events 
@@ -178,14 +111,91 @@ def generate(events = 24000, fix_array = fix_array, verbose = False):
         print("Time taken to generate data:", t1-t0)
     return  Coef0, signal_events
 
+'''
+def nll(signal_coeffs):
+    """
+    Return negative of the log likelihood for given events based on particular amplitude coefficients
 
-def minuitfit(Coef0, signal_events,fix_array, Ncall=1000,verbose = False):
+    Args:
+        coeffs: List of scalar coefficient tensors
+        events: Tensor of shape (N, 4) with axis 1 representing params [q2, cos_theta_k, cos_theta_l, phi]
+
+    Returns:
+        Scalar: negative log likelihood
+
+    """
+    return bmfs.nll(signal_coeffs, signal_events)
+'''
+
+
+def plot_profiles(m , Coef0 ,coeffout,fix_array , amplitude_latex_names, save_path , show = True, save = False):
+    #First find index of non fixed parameters 
+    ind=[]
+    for i in range(len(fix_array)):
+        if fix_array[i]==0 : 
+            ind.append(int(i))
+
+    #Now ,plot nll profile for given parameters
+    LaTex=LaTex_labels(amplitude_latex_names)
+    Title=Standard_labels(amplitude_names)    
+
+    #Different case if only one plot or more
+    if len(ind)==1 : 
+        fig, ax = plt.subplots()
+        i0=ind[0]
+        X , Y =m.profile(m.parameters[i0])
+        ax.plot(X , Y , label='NLL')
+        ymin, ymax = ax.get_ylim()
+        ax.vlines(Coef0[i0] , ymin , ymax , label='MC value')
+        ax.vlines(m.values[i0] , ymin , ymax , label='Fitted value' , color='r')
+        X , Y =m.profile(m.parameters[i0])
+        ax.set(xlabel=LaTex[i0], ylabel=r'NLL')
+        Xmin , Xmax = m.values[i0]-m.errors[i0] ,  m.values[i0]+m.errors[i0] 
+        ax.axvspan(Xmin, Xmax, alpha=0.1, color='red' , label= r'$\pm \sigma$')
+        ax.set_title('Fit for '+LaTex[i0]+'with N='+str(Ncall)+'\n Expected Value:'+str(Coef0[i0])+' Actual Value:'+str(coeffout[i0]))
+        ax.legend()
+        titre = Title[i0]
+
+        #Save figures of save= True 
+        if save : 
+            plt.savefig(save_path+titre+'.png')
+    else : 
+        for i in ind:
+            #Draw profile along parameter parameters[i]
+            X , Y =m.profile(m.parameters[i])
+            fig, ax = plt.subplots()
+            ax.plot(X , Y , label='NLL')
+            ymin, ymax = ax.get_ylim()
+            ax.vlines(Coef0[i] , ymin , ymax , label='MC value')
+            ax.vlines(m.values[i] , ymin , ymax , label='fitted value' , color='r')
+            ax.set(xlabel=LaTex[i], ylabel=r'NLL')
+            Xmin , Xmax = m.values[i]-m.errors[i] ,  m.values[i]+m.errors[i] 
+            ax.axvspan(Xmin, Xmax, alpha=0.1, color='red' , label= r'$\pm \sigma$')
+            ax.set_title('Expected Value:'+str(format(Coef0[i], '.5f'))+' Actual Value:'+str(format(coeffout[i], '.5f')))
+            ax.legend()
+
+            #Save figures of save= True 
+            if save:
+                plt.savefig(save_path+Title[i]+'.png')
+
+        #Title = 'Likelihood profiles for '+str(len(ind))+' parameters'
+    plt.tight_layout()
+
+    if show:
+        plt.show()
+  
+def minuitfit(Coef0, signal_events,fix_array, Ncall=10000,verbose = False):
     A=bmf.coeffs.fit(bmf.coeffs.fit_initialization_scheme_default)
     Coef_INIT=[A[i].numpy() for i in range(len(A))]
     Coef_INIT=set_coef(Coef_INIT, Coef0, fix_array)
+    
+    def nll_iminuit(signal_coeffs):
+        return bmfs.nll(signal_coeffs, signal_events)
+
     if verbose:
+        print("Coeffs used for MC:", Coef0)
         print("Initial coeffs for minuit fit:", Coef_INIT)
-    m = Minuit.from_array_func(nll,Coef_INIT, fix= fix_array , errordef=1, pedantic = False)
+    m = Minuit.from_array_func(nll_iminuit,Coef_INIT, fix= fix_array , errordef=0.5, pedantic = False)
     t0=time.time()
     m.migrad( ncall=Ncall)
     t1=time.time()
@@ -195,39 +205,18 @@ def minuitfit(Coef0, signal_events,fix_array, Ncall=1000,verbose = False):
     Coef_OUT=[m.values[i] for i in range(len(m.values))]
     if verbose:
         print(Coef_OUT)
-        print('NLL for final set of coeffs:' , nll(Coef_OUT))
+        print('NLL for final set of coeffs:' , nll_iminuit(Coef_OUT))
     return m, Coef0, Coef_OUT
 
+def array_out(m):
+    n=len(m.parameters)
+    OUT=np.zeros((n , 2))
+    for i in range(n):
+        OUT[i , :] = [ m.values[i] , m.errors[i]]
+    return OUT
 
 
-
-'''
-for i in tqdm(range(48)):
-    array =[
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        1,1,1,
-        ]
-    array[i] = 0
-    coeffs, signal_events = generate(fix_array = array)
-    m , Coef0, Coef_OUT = minuitfit(coeffs,signal_events,fix_array = array, Ncall = 1000)
-    plot_fixed(m , Coef0, array , amplitude_latex_names,Coef_OUT,show = False, save=True)
-'''
-
-array =[
+array=[
 0,0,0,
 0,0,0,
 0,0,0,
@@ -246,7 +235,11 @@ array =[
 0,1,1,
 ]
 
-
-coeffs, signal_events = generate(fix_array = array)
-m , Coef0, Coef_OUT = minuitfit(coeffs,signal_events,fix_array = array, Ncall = 100 , verbose=True)
-plot_fixed(m , Coef0, array , amplitude_latex_names,Coef_OUT,show = True, save=False)
+'''
+coeffs, signal_events = generate_SM(fix_array = array)
+m, Coef0, Coef_OUT = minuitfit(coeffs, signal_events,array, Ncall=10000,verbose = True)
+OUT=array_out(m)
+print(OUT)
+'''
+#save_path = './Minuit/Test/'
+#plot_profiles(m , Coef0 ,Coef_OUT,fix_array , amplitude_latex_names, save_path , show = False, save = False)
