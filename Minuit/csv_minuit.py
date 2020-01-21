71ff63c468b81a4aac5a91800f04d1c02b8cfa46
kamathrohan <rohan.kamath9@gmail.com>
2020-01-21 18:31:34 +0000

    new script for csv dumping written

---
+++ Minuit/csv_minuit.py
@@ -0,0 +1,38 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import csv 
+import pandas as pd
+from test_iminuit import generate_SM, minuitfit, array_out , LaTex_labels , Standard_labels, array_out_long
+from tqdm import tqdm 
+
+
+iterations=10
+
+
+array=[
+0,0,0,
+0,0,0,
+0,0,0,
+0,0,0,
+0,0,0,
+0,0,0,
+0,0,0,
+1,1,1,
+0,0,0,
+1,1,1,
+1,1,1,
+1,1,1,
+0,1,1,
+0,1,1,
+0,1,1,
+0,1,1,
+]
+for i in tqdm(range(iterations)):
+    coeffs, signal_events = generate_SM(fix_array = array)
+    m, Coef0, Coef_OUT = minuitfit(coeffs, signal_events , array,verbose = False)
+    OUT=array_out_long(m)
+    with open(r"./Minuit/Test_stats/data.csv", 'a') as data:
+        writer =csv.writer(data)
+        writer.writerow(OUT) 
+    data.close()
+
