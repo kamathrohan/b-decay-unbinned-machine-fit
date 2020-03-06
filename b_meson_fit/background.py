27126a109a91b718556825bfb06c94236adb194e
kamathrohan <rohan.kamath9@gmail.com>
2020-03-06 15:38:54 +0000

    changes to background

--- b_meson_fit/background.py
+++ b_meson_fit/background.py
@@ -12,9 +12,6 @@ coeffs = [
 
 def pdf(coeffs, events):
     [q2, cos_theta_k, cos_theta_l, phi] = tf.unstack(events, axis=1)
-    return (coeffs[0][0] + q2*coeffs[0][1]
-            +coeffs[1][0] + cos_theta_k*coeffs[1][1]    
-            +coeffs[2][0] + cos_theta_l*coeffs[2][1]
-            +coeffs[3][0] + phi*coeffs[3][1])
+    return ((coeffs[0][0] + q2*coeffs[0][1])*(coeffs[1][0] + cos_theta_k*coeffs[1][1])*( coeffs[2][0] + cos_theta_l*coeffs[2][1])*(coeffs[3][0] + phi*coeffs[3][1]))
 
             
